0378fcf52b58931c908a2e626a54e54f6efa9694
ZIGuser <ziguser@localhost.localdomain>
2025-07-15 08:51:46 -0300

    removed unnecessary files

--- build/Desktop_Qt_6_9_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_kde_splash_creator_FILE /home/ziguser/Documentos/sources/qt_cpp/kde_splash_creator/build/Desktop_Qt_6_9_1-Debug/kde_splash_creator)
-set(__QT_DEPLOY_TARGET_kde_splash_creator_TYPE EXECUTABLE)
